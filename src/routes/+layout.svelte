d705d478cd344756947d99655f4a9c5b46837c0a
Micha Mailaender <micha@mailaender.me>
2025-06-03 15:28:24 +0200

    feat: add Tailwind plugins for forms and typography with configuration updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
 	import { setupConvexAuth } from '$lib/sveltekit';
-
 	import '../app.css';
 
 	let { children, data } = $props();
 
 	setupConvexAuth({
 		getServerState: () => data.authState,
-		options: {
-			verbose: true
-		}
+		options: { verbose: true }
 	});
 </script>
 
